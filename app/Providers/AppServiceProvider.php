bdf962267550013bb485af9e5b9b604885ae4700
ebadnn <ebadullah.naveed@invisionsolutions.ca>
2023-02-01 21:09:08 +0500

    force https for staging and production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV') == 'stage' || env('APP_ENV') == 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
