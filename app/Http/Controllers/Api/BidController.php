fb49ca3702eb9dfbed1fc7dab478e718d1da30d6
Ebadullah-Naveed <ebad.nn@gmail.com>
2023-02-09 03:27:40 +0500

    unique value from bid

--- app/Http/Controllers/Api/BidController.php
+++ app/Http/Controllers/Api/BidController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\{Category,Product,ProductBid};
+use Carbon\Carbon;
 
 class BidController extends Controller
 {
     public function index(Request $request)
     {
-        $bid = ProductBid::where('user_id',auth()->user()->id)->with('product.images')->get();
+        $bid = ProductBid::where('user_id',auth()->user()->id)->with('product.images')->orderBy('amount','desc')->get()->unique('product_id');
         return $this->response(true,$bid,'Bid fetched successfully',200);
     }
 
     public function bidProduct(Request $request)
     {
         $product = Product::where('id',$request->product_id)->first();
+        if(Carbon::now() > Carbon::parse($product->end_datetime))
+        {
+            return $this->response(false,null,'Bid is now closed',422);
+        }
         if(!$product)
         {
             return $this->response(false,null,'Invalid Product Id',422);
@@ -28,6 +33,12 @@ class BidController extends Controller
                 'product_id' => $request->product_id,
                 'amount' => $request->amount
             ]);
+            $newDateTime = Carbon::now()->addMinutes(4);
+            if($newDateTime > Carbon::parse($product->end_datetime))
+            {
+                $newEndTime = Carbon::parse($product->end_datetime)->addMinutes(5);
+                Product::where('id',$request->product_id)->update(['end_datetime' => $newEndTime]);
+            }
         }
         else if($request->amount >= $product->min_increment && $request->amount <= $product->max_increment)
         {
@@ -36,6 +47,12 @@ class BidController extends Controller
                 'product_id' => $request->product_id,
                 'amount' => $request->amount
             ]);
+            $newDateTime = Carbon::now()->addMinutes(4);
+            if($newDateTime > Carbon::parse($product->end_datetime))
+            {
+                $newEndTime = Carbon::parse($product->end_datetime)->addMinutes(5);
+                Product::where('id',$request->product_id)->update(['end_datetime' => $newEndTime]);
+            }
         }
         else
         {
